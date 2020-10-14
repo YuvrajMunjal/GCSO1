4cd403d345e9a094034c29edd8a5a1af39e1f9c4
YuvrajMunjal <40139552+YuvrajMunjal@users.noreply.github.com>
2020-10-14 19:05:23 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ function setup() {
   wall = createSprite(1500,200,60,height/2);
   speed = random(55,90)
   weight = (400,1500)
-  deformation = 0.5*weight*speed*speed/22500
+ 
 }
 
 function draw() {
@@ -16,7 +16,7 @@ function draw() {
   
   car.velocityX = speed;
   console.log(deformation)
-
+  deformation = 0.5*weight*speed*speed/22500
 
   if(car.collide(wall)){
     speed = 0   
